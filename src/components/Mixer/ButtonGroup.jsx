f71d13b66a85fd1e8444a87967267889b7241836
Andrew Wong <andrew.j.wong@outlook.com>
2020-11-25 18:21:24 +1100

    styled-components perf improvements

--- src/components/Mixer/ButtonGroup.jsx
+++ src/components/Mixer/ButtonGroup.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 import styled from 'styled-components'
 
-export default function (props) {
-  const ButtonGroup = styled.div`
-    display: grid;
-    grid-template-columns: repeat(${props.cols || 1}, 1fr);
-    justify-items: center;
-    grid-column-gap: 5px;
-    padding: 0 5px;
-  `
+const ButtonGroup = styled.div`
+  display: grid;
+  grid-template-columns: repeat(${props => props.cols || 1}, 1fr);
+  justify-items: center;
+  grid-column-gap: 5px;
+  padding: 0 5px;
+`
 
-  return <ButtonGroup style={{ ...props.style }}>{props.children}</ButtonGroup>
+export default function (props) {
+  return <ButtonGroup cols={props.cols} style={{ ...props.style }}>{props.children}</ButtonGroup>
 }
--- src/components/Mixer/Label.jsx
+++ src/components/Mixer/Label.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 import styled from 'styled-components'
 
+const Label = styled.div`
+  text-align: center;
+  color: ${props => props.color || '#FFF'};
+  background-color: ${props => props.backgroundColor || '#000'};
+`
+
 export default function (props) {
-  const Label = styled.div`
-    text-align: center;
-    color: ${props.color || '#FFF'};
-    background-color: ${props.backgroundColor || '#000'};
-  `
-  return <Label>{props.children}</Label>
+  return <Label color={props.color} backgroundColor={props.backgroundColor} >{props.children}</Label>
 }
